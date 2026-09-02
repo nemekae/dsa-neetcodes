6eb97c4d900041e7fd6860067f3cf5bfbfefe969
Emeka N. <meenwachukwu@gmail.com>
2026-09-02 21:05:45 +0100

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+
+        matrix.reverse()
+
+        for r in range(n):
+            for c in range(r, n):
+                matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+        return
