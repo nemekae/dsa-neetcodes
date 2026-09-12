049cd6bff4b7807b9b997c6549bc192fcd610ee5
Emeka N. <meenwachukwu@gmail.com>
2026-09-12 18:29:46 +0100

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def __init__(self):
+        self.delimiter = "Penis"
+
+    # Use something as a delimeter in the list
+    def encode(self, strs: List[str]) -> str:
+        # Initialize encoded str
+        encoded_str = ""
+
+        # Build out the str
+        for string in strs: 
+            encoded_str += string + self.delimiter
+        
+        return encoded_str
+
+    def decode(self, s: str) -> List[str]:
+        # Initialize decoded list
+        decoded_list = s.split(self.delimiter)
+
+        return decoded_list[:len(decoded_list)-1]
